81b8695d73f926163309957388ec310d1159eb9b
Jared Rewerts <jaredrewerts@gmail.com>
2017-11-10 17:03:24 -0700

    Reorganized the web app.

--- server.js
+++ server.js
@@ -3,8 +3,7 @@
 
 var express = require('express');
 var app = express();
-app.use(express.static('public'));
-app.use(express.static('node_modules/@livechat/livechat-visitor-sdk/dist'));
+app.use(express.static('app'));
 
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
