e9d55fd98354c6ebd3feef86a40db21dba0fb8c8
Nathan Kaefer <stormkaefer@gmail.com>
2017-12-18 10:28:52 -0700

    No mutation observer code, timeout works for our needs

--- src/extension/pop-up.js
+++ src/extension/pop-up.js
@@ -11,28 +11,6 @@ var chat_id;
 var loaded = false;
 const tracking = 'https:' + 'cdn.livechatinc.com/tracking.js';
 
-// Add mutationobserver code, to check if we have run into an error
-var observer = new MutationObserver(function(mutations) {
-	// For the sake of...observation...let's output the mutation to console to see how this all works
-	mutations.forEach(function(mutation) {
-		console.log(mutation.type);
-	});    
-});
- 
-// Notify me of everything!
-var observerConfig = {
-	attributes: true, 
-	childList: true, 
-    characterData: true,
-    subtree: true
-};
- 
-// Node, config
-// In this case we'll listen to all changes to body and child nodes
-var targetNode = document.body;
-observer.observe(targetNode, observerConfig);
-
-
 
 // Add livechat init code
 chrome.storage.local.get('VC_USER',
